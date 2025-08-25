423d7306682d83e4c52cb84cf54d1750ae6b16a1
noone17 <116145409+noone17@users.noreply.github.com>
2025-08-25 21:34:59 +0530

    Update app.js v1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("This is for CI/CD project 🚀");
+    res.send("This is for CI/CD project v.1");
 });
 
 app.listen(PORT, () => {
